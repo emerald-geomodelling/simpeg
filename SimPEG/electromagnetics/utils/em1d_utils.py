fba7152334d342201435a51c644ec7c41129ae3e
Martin Panzner <mp@emrld.no>
2022-02-10 09:42:54 +0100

    modification to plot res. instead of cond.

--- SimPEG/electromagnetics/utils/em1d_utils.py
+++ SimPEG/electromagnetics/utils/em1d_utils.py
@@ -15,13 +15,17 @@ from matplotlib.colors import LogNorm
 import warnings
 
 
-def plot_layer(sig, mesh, xscale='log', ax=None, showlayers=False, xlim=None,**kwargs):
+
+def plot_layer(sig, mesh, xscale='log', ax=None, showlayers=False, xlim=None, plotSig=True, **kwargs):
     """
         Plot Conductivity model for the layered earth model
     """
-
+    
     z_grid = mesh.vectorNx
     n_sig = sig.size
+    if not plotSig:
+        sig = 1./sig
+    
     sigma = np.repeat(sig, 2)
     z = []
     for i in range(n_sig):
@@ -32,29 +36,34 @@ def plot_layer(sig, mesh, xscale='log', ax=None, showlayers=False, xlim=None,**k
         sig_max = sig[~np.isnan(sig)].max()*2
     else:
         sig_min, sig_max = xlim
-
+    
     if xscale == 'linear' and sig.min() == 0.:
         if xlim == None:
             sig_min = -sig[~np.isnan(sig)].max()*0.5
             sig_max = sig[~np.isnan(sig)].max()*2
-
+    
     if ax==None:
         plt.xscale(xscale)
         plt.xlim(sig_min, sig_max)
         plt.ylim(z.min(), z.max())
-        plt.xlabel('Conductivity (S/m)', fontsize = 14)
-        plt.ylabel('Depth (m)', fontsize = 14)
+        if plotSig:
+            plt.xlabel('Conductivities (S/m)', fontsize = 14)
+        else:
+            plt.xlabel('Resistivity (Ohm.m)', fontsize = 14)
         plt.ylabel('Depth (m)', fontsize = 14)
         if showlayers == True:
             for locz in z_grid:
                 plt.plot(np.linspace(sig_min, sig_max, 100), np.ones(100)*locz, 'b--', lw = 0.5)
         return plt.plot(sigma, z, 'k-', **kwargs)
-
+    
     else:
         ax.set_xscale(xscale)
-        ax.set_xlim(sig_min, sig_max)
+        ax.set_xlim(sig_min*0.9, sig_max*1.1)
         ax.set_ylim(z.min(), z.max())
-        ax.set_xlabel('Conductivity (S/m)', fontsize = 14)
+        if plotSig:
+            plt.xlabel('Conductivities (S/m)', fontsize = 14)
+        else:
+            plt.xlabel('Resistivity (Ohm.m)', fontsize = 14)
         ax.set_ylabel('Depth (m)', fontsize = 14)
         if showlayers == True:
             for locz in z_grid:
